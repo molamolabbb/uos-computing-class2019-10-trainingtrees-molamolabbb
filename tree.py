285e3947838857399cb543e8ad400072a505b3be
Ian J. Watson <ian.james.watson@cern.ch>
2019-05-29 21:13:17 +0900

    starter pack

---
+++ tree.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from math import log2
+
+def is_tree(thing):
+    pass
+
+def classify(tree, x):
+    pass
+
+def tree_accuracy(tree, x, y):
+    pass
+
+# --- entropy ---
+
+def entropy(class_probabilities):
+    pass
+
+def class_probabilities(labels):
+    pass
+
+def data_entropy(labeled_data):
+    pass
+
+def partition_entropy(subsets):
+    pass
+
+
+# --- build tree ---
+
+def partition_by(inputs, attribute, threshold):
+    pass
+
+def partition_entropy_by(inputs, attribute, threshold):
+    pass
+
+def best_partition_entropy(inputs):
+    pass
+
+def build_tree_id3(inputs, max_height, current_height=0):
+    pass
+
+def is_tree(thing):
+    pass
+
+def classify(tree, x):
+    pass
+
+def tree_accuracy(tree, x, y):
+    pass
